2090584963041d173bd089a1031654d6ed6ecca0
Camille Marques <marquescamille@gmail.com>
2022-03-25 15:42:28 +0100

    app

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,28 @@
 class BookmarksController < ApplicationController
+
+before_action :set_list, only:[:new, :create]
+
   def new
-    @bookmarks = Bookmark.new
+    @bookmark = Bookmark.new
   end
+
+  def create
+    @bookmark = Bookmark.new(bookmarks_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+  private
+
+  def bookmarks_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lists, only: [:index, :show, :create, :destroy] do
-    resources :bookmarks
+    resources :bookmarks, only: [:create, :new ]
 
   end
 end
